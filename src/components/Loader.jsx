270f907a953a8c6ce1b995d31eb94a36edbf87a0
ashpakhyd <ashpakhyd@gmail.com>
2026-01-09 01:44:11 +0530

    loader added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import "./loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+    </div>
+  );
+}
