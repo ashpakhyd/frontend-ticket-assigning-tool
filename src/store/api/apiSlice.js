bd0aa3ca56de5213ed04f5b6623811b390792c8b
ashpakhyd <ashpakhyd@gmail.com>
2026-02-05 17:41:57 +0530

    FE Admin panel: base url reverted

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://backend-ticket-assigning-tool.onrender.com/api",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) headers.set("authorization", `Bearer ${token}`);
